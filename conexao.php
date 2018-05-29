ce7d11506c4de84ed955fc9ee4e4ceba1298222f
Derek <derekgodoy@gmail.com>
2018-05-29 18:57:53 -0300

    Edita db para umbler

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$db_name = 'projeto';
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
+$db_name = 'derekluan';
+$hostname = 'mysql427.umbler.com:41890';
+$username = 'derekluan';
+$password = 'derekluan123';
 $opcoes = array(
 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
 );
