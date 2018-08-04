45c6f1f8438d2d403f6c2519fa2040fcd0e2f566
Derek <derekgodoy@gmail.com>
2018-08-04 09:25:52 -0300

    Corrige DB

--- conexao.php
+++ conexao.php
@@ -1,13 +1,8 @@
 <?php
-// $db_name = 'derekluan';
-// $hostname = 'mysql427.umbler.com';
-// $username = 'derekluan';
-// $password = 'derekluan123';
-
-$db_name = 'projeto';
-$hostname = ' 127.0.0.1 ';
-$username = 'root';
-$password = '';
+$db_name = 'derekluan';
+$hostname = 'mysql427.umbler.com';
+$username = 'derekluan';
+$password = 'derekluan123';
 $opcoes = array(
 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
 );
